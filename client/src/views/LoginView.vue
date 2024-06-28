0adf21ebb19fd6819c3122ad1b9a59f70d768e83
Emil <emil_mange@hotmail.com>
2024-06-28 15:21:52 +0200

    vue setup and temp login page

---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="h-screen flex items-center justify-center">
+        <img src="../assets/pladder-logo.png"/>
+        <login-form></login-form>
+    </div>
+  </template>
+  
+  <script>
+  // @ is an alias to /src
+  import LoginForm from '@/components/login/LoginForm.vue'
+  
+  export default {
+    name: 'LoginView',
+    components: { LoginForm }
+  }
+  </script>
+  
